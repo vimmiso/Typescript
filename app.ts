e164aa35183a126ef5867db82b035e03d6cfdba9
vimmiso <sirarkumar@gmail.com>
2020-02-28 20:07:51 +0530

    after instruction.

--- app.ts
+++ app.ts
@@ -2,10 +2,12 @@ interface Person{
     Name:string;
     Age:number;
 }
+
 class School{
     public lists:Student[]=[];
     schoolName:string="abc";
 }
+
 class Student extends School implements Person{
 
     constructor(public Name:string,public Age:number,public Class:string,public FavSubject:string,public SchoolName:string){
@@ -17,48 +19,40 @@ var lists:Student[]=[];
 
 function startGame(){
   
-       const input = <HTMLInputElement>document.getElementById('name');
-        const input2 = <HTMLInputElement>document.getElementById('class');
-        const input3 = <HTMLInputElement>document.getElementById('age');
-        const input4 = <HTMLInputElement>document.getElementById('favsubject');
-        const input5 = <HTMLInputElement>document.getElementById('schoolName');
-        var stud = new Student(input.value,Number(input4.value),input2.value,input3.value,input5.value);
+    const input = <HTMLInputElement>document.getElementById('name');
+    const input2 = <HTMLInputElement>document.getElementById('class');
+    const input3 = <HTMLInputElement>document.getElementById('age');
+    const input4 = <HTMLInputElement>document.getElementById('favsubject');
+    const input5 = <HTMLInputElement>document.getElementById('schoolName');
+    var stud = new Student(input.value,Number(input4.value),input2.value,input3.value,input5.value);
          
 
 
         
-            if( input.checkValidity() && input2.checkValidity()  && input3.checkValidity() && input4.checkValidity()  && input5.checkValidity() ){
-            lists.push(stud);
-            console.log(lists.length);
-           console.log(stud);
+    if( input.checkValidity() && input2.checkValidity()  && input3.checkValidity() && input4.checkValidity()  && input5.checkValidity() ){
+        lists.push(stud);
+        console.log(lists.length);
+        console.log(stud);
+    }else{
         
-            }else{
-           
-                if( !input.checkValidity() ){
-                    alert('Please fill the correct Name...');
-
-                    
-                }
-                if ( !input2.checkValidity() ) {
-                    alert('Please fill the corrct Class.');
-                    
-                }
-                 if( !input3.checkValidity()){
-                    alert('Please fill the correct Age...');
-                    
-                }
-                if(!input4.checkValidity() ){
-                    alert('Please fill the correct Subject Name...');
-                    
-                }
-                 if( !input5.checkValidity()){
-                    alert('Please fill the correct School Name...');
-                   
-        
-                }
-
+        if( !input.checkValidity() ){
+            alert('Please fill the correct Name...');          
         }
+        if ( !input2.checkValidity() ) {
+            alert('Please fill the corrct Class.');
+        }
+            if( !input3.checkValidity()){
+            alert('Please fill the correct Age...');   
+        }
+        if(!input4.checkValidity() ){
+            alert('Please fill the correct Subject Name...');
+        }
+        if( !input5.checkValidity()){
+            alert('Please fill the correct School Name...');
+        }
+
     }
+ }
         
 
 
@@ -67,9 +61,9 @@ function startGame(){
    
   
 
-   function logPlayer(name:string){
+function logPlayer(name:string){
        console.log('Name game starting for player:',name);
-   }
+}
 
 
 document.getElementById('searchbutton')?.addEventListener('click',evt =>{
@@ -78,33 +72,31 @@ document.getElementById('searchbutton')?.addEventListener('click',evt =>{
     if(regSchool.test(input6.value)===false){
         alert('School Name is in  Wrong Format.');
     }
-})
+});
 
-    function updateList():void{
-        const input6 = <HTMLInputElement>document.getElementById('search');
-        var regSchool =new RegExp('[A-Za-z ]+');
-        if(regSchool.test(input6.value)===false){
-            alert('School Name is in  Wrong Format.');
-        }else{
-            let output:string ='<h2>List</h2>';
+function updateList():void{
+    const input6 = <HTMLInputElement>document.getElementById('search');
+    var regSchool =new RegExp('[A-Za-z ]+');
+    if(regSchool.test(input6.value)===false){
+        alert('School Name is in  Wrong Format.');
+    }else{
+        let output:string ='<h2>List</h2>';
 
-            // console.log(lists.length);
-            for(let index=0;index<lists.length;index++){
-                const student:Student=lists[index];
+        for(let index=0;index<lists.length;index++){
+            const student:Student=lists[index];
             if(input6.value===lists[index].SchoolName){
                 output+='<li>';
                 output+=(lists[index]).Name;
                 output+='</li>';
             }
-               
-            }
-    
-            const displayList:HTMLElement=<HTMLElement>document.getElementById('displaylist');
-            displayList.innerHTML=output;
         }
-        
-        
+
+        const displayList:HTMLElement=<HTMLElement>document.getElementById('displaylist');
+        displayList.innerHTML=output;
     }
+    
+    
+}
    
 
  
